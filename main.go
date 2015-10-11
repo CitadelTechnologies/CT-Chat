bb0833c6c1fe70d8f62181b380bda6e7862a83bf
Axel Venet <kern046@gmail.com>
2015-10-11 14:08:53 +0200

    Add basic webserver to communicate with

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import(
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+
+	httpDone := make(chan bool)
+	go listenHttp(httpDone)
+	<-httpDone
+}
+
+func listenHttp(done chan bool) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Closing server ...")
+		done <- true
+	})
+	http.ListenAndServe(":5515", nil)
 }
