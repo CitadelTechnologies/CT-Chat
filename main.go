021696aacecbe76ae5b277825795f8bbabe448d4
Axel Venet <kern046@gmail.com>
2015-10-12 13:20:32 +0200

    Recognize used usernames and forbid duplication

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"time"
+	"crypto/md5"
+	"io"
 )
 
-func main() {
+type (
+	User struct {
+		username string
+		token string
+	}
+	Users map[string]User
+)
 
+var users Users
+
+func main() {
+	users = make(Users, 0)
 	httpDone := make(chan bool)
 	go listenHttp(httpDone)
 	<-httpDone
@@ -22,8 +35,16 @@ func listenHttp(done chan bool) {
 		username := r.FormValue("username")
 		if username == "" {
 			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprintf(w, "You must have an username to sign in")
 			return
 		}
+		if !isUsernameAvailable(username) {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprintf(w, "This username is already taken")
+			return
+		}
+		token := generateToken(username)
+		users[token] = User{username: username, token: token}
 		fmt.Fprintf(w, "Hello " + username)
 	})
 	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
@@ -31,4 +52,20 @@ func listenHttp(done chan bool) {
 		done <- true
 	})
 	http.ListenAndServe(":5515", nil)
+}
+
+func isUsernameAvailable(username string) bool {
+	for _, user := range users {
+		if(user.username == username) {
+			return false
+		}
+	}
+	return true
+}
+
+func generateToken(username string) string {
+	hash := md5.New()
+	io.WriteString(hash, username)
+	io.WriteString(hash, time.Now().Format(time.UnixDate))
+	return string(hash.Sum(nil))
 }
