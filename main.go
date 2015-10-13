6e79838fce08b610738a909ecd3265c0960576d5
Axel Venet <kern046@gmail.com>
2015-10-13 13:20:26 +0200

    Add token notion and respond JSON instead of raw text

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import(
-	"fmt"
 	"net/http"
 	"time"
 	"crypto/md5"
 	"io"
+	"encoding/json"
+	"encoding/hex"
 )
 
 type (
@@ -14,6 +15,10 @@ type (
 		token string
 	}
 	Users map[string]User
+	Communication struct {
+		Token string `json:"token"`
+		Message string `json:"message"`
+	}
 )
 
 var users Users
@@ -35,20 +40,33 @@ func listenHttp(done chan bool) {
 		username := r.FormValue("username")
 		if username == "" {
 			w.WriteHeader(http.StatusForbidden)
-			fmt.Fprintf(w, "You must have an username to sign in")
+			c := Communication{Message: "You must have an username to sign in"}
+			if err := json.NewEncoder(w).Encode(&c); err != nil {
+				panic(err)
+			}
 			return
 		}
 		if !isUsernameAvailable(username) {
 			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprintf(w, "This username is already taken")
+			c := Communication{Message: "This username is already taken"}
+			if err := json.NewEncoder(w).Encode(&c); err != nil {
+				panic(err)
+			}
 			return
 		}
 		token := generateToken(username)
 		users[token] = User{username: username, token: token}
-		fmt.Fprintf(w, "Hello " + username)
+		c := Communication{Token: token, Message: "You are connected"}
+		if err := json.NewEncoder(w).Encode(&c); err != nil {
+			panic(err)
+		}
 	})
 	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Closing server ...")
+		w.WriteHeader(http.StatusUnauthorized)
+		c := Communication{Message: "Closing server"}
+		if err := json.NewEncoder(w).Encode(&c); err != nil {
+			panic(err)
+		}
 		done <- true
 	})
 	http.ListenAndServe(":5515", nil)
@@ -67,5 +85,5 @@ func generateToken(username string) string {
 	hash := md5.New()
 	io.WriteString(hash, username)
 	io.WriteString(hash, time.Now().Format(time.UnixDate))
-	return string(hash.Sum(nil))
+	return hex.EncodeToString(hash.Sum(nil))
 }
